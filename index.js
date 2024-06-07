5903aa5bd9dfc714399d9993f2b90006b2b885fe
hakuu303 <xde3330g@gmail.com>
2024-06-06 21:47:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesbtn = document.querySelector("#yesbtn");
+
+yesbtn.addEventListener("click",function () {
+    alert("me perdonas? shiii 🥰te amo bb")
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
